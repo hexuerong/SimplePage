bb5d6bfe885c93218847d20a227096481f724d25
hexuerong-7f <hexuerong@7fgame.com>
2018-07-16 17:56:03 +0800

    vue加入less-loader

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 #app1 {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,5 +26,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  .list-group{
+    background-color: #cccccc;
+    color: red;
+  }
 }
 </style>
