493c2ff4089b3818596294f48257ae135b99a296
hexuerong-7f <hexuerong@7fgame.com>
2018-12-14 10:29:20 +0800

    菜单树

--- src/components/tree.vue
+++ src/components/tree.vue
@@ -1,10 +1,17 @@
 <template>
+<div>
     <el-tree 
+    ref="tree"
     :data="data" 
     :props="defaultProps" 
+    :indent='17'
     :expand-on-click-node="false" 
-    @node-click="handleNodeClick">
+    @node-click="handleNodeClick"
+    :highlight-current="true">
     </el-tree>
+    <el-button type='text' @click="clickButton1">取消选中</el-button>    
+    <el-button type='text' @click="clickButton2">选中上次选中</el-button>        
+</div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -50,17 +57,60 @@ export default {
             defaultProps: {
                 children: 'children',
                 label: 'label'
+            },
+            currentItem: null,
+            lastItem: {
+                label: '一级 3',
+                children: [{
+                    label: '二级 3-1',
+                    children: [{
+                        label: '三级 3-1-1'
+                    }]
+                }, {
+                    label: '二级 3-2',
+                    children: [{
+                        label: '三级 3-2-1'
+                    }]
+                }]
             }
         }
     },
     methods: {
         handleNodeClick (data) {
-
+            this.currentItem = data;
+            // this.lastItem = data;
+        },
+        clickButton1 (e){
+            this.currentItem = null;
+            this.$refs.tree.selectNode(null);
+        },
+        clickButton2 (e){
+            // debugger
+            console.log(this.currentItem);
+            console.log(this.lastItem);            
+            if(this.currentItem){
+                this.$refs.tree.selectNode(this.currentItem);
+            }else{
+                console.log('in');
+                this.$refs.tree.selectNode(this.lastItem);     
+            }           
         }
     }
 }
 </script>
 
 <style lang="less" scoped>
-
+/* .el-tree--highlight-current{
+    .el-tree-node{
+        &.is-current{
+            &>.el-tree-node__content{
+                background-color: #4d4d80!important;
+                color: #ffffff;
+            }
+        }
+    }
+} */
+.el-tree--highlight-current .el-tree-node.is-current>.el-tree-node__content{
+    background-color: #4d4d80!important;
+}
 </style>
